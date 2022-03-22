40c41243daf88e5cc282b3ad48941b421bfb3624
TimothyGCY <timothy.gcy@outlook.com>
2022-03-22 23:53:22 +0800

    use image representation for first dice

--- DiceRoll/app/src/main/java/glab/bleckshiba/diceroll/MainActivity.kt
+++ DiceRoll/app/src/main/java/glab/bleckshiba/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package glab.bleckshiba.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import glab.bleckshiba.diceroll.models.Dice
 
@@ -12,7 +13,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dice2: Dice
     private lateinit var dice3: Dice
 
-    private lateinit var txtDice1: TextView
+    private var dice1Value = R.drawable.dice_1
+
+    private lateinit var ivDice1: ImageView
     private lateinit var txtDice2: TextView
     private lateinit var txtDice3: TextView
 
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtDice1 = findViewById(R.id.txt_dice1)
+        ivDice1 = findViewById(R.id.iv_dice1)
         txtDice2 = findViewById(R.id.txt_dice2)
         txtDice3 = findViewById(R.id.txt_dice3)
 
@@ -35,13 +38,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setText() {
-        txtDice1.text = dice1.toString()
+        ivDice1.setImageResource(dice1Value)
         txtDice2.text = dice2.toString()
         txtDice3.text = dice3.toString()
     }
 
     private fun rollDice() {
-        dice1.roll()
+        when (dice1.roll()) {
+            1 -> dice1Value = R.drawable.dice_1
+            2 -> dice1Value = R.drawable.dice_2
+            3 -> dice1Value = R.drawable.dice_3
+            4 -> dice1Value = R.drawable.dice_4
+            5 -> dice1Value = R.drawable.dice_5
+            6 -> dice1Value = R.drawable.dice_6
+        }
         dice2.roll()
         dice3.roll()
 
